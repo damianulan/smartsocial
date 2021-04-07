3200d4a86e7799fff86929e2a72cd2b024e5d893
damianulan <damian.ulan@protonmail.com>
2021-04-07 20:04:11 +0200

    Fixing minor issues #1

--- 404.php
+++ 404.php
@@ -1,18 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="" />
-    <meta name="keywords" content="" />
-	<title>Whoops - 404 Site not Found</title>    
-    <link rel="stylesheet" href="css/main.min.css">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/color.css">
-    <link rel="stylesheet" href="css/responsive.css">
+<?php include 'head/header.php'; ?>
 
-</head>
 <body>
 <!--<div class="se-pre-con"></div>-->
 <div class="theme-layout">
