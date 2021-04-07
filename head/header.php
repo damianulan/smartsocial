da6bb3871c130763a87798ed4c2d46b23f819058
damianulan <damian.ulan@protonmail.com>
2021-04-07 21:21:44 +0200

    Adding user's timeline

--- head/header.php
+++ head/header.php
@@ -3,7 +3,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="" />
     <meta name="keywords" content="" />
-	<title>SmartSocial App - <?php echo $pagetitle; ?></title>
+	<title>SmartSocial - <?php echo $pagetitle; ?></title>
     
     <link rel="stylesheet" href="css/main.min.css">
     <link rel="stylesheet" href="css/style.css">
